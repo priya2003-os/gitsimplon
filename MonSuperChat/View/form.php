b758aa3880d1299198f02427e425cbeeb5011779
priya2003-os <78696392+priya2003-os@users.noreply.github.com>
2021-06-02 15:17:41 +0200

    Add files via upload
    Chat/formulaire

---
+++ MonSuperChat/View/form.php
@@ -0,0 +1,28 @@
+
+
+<form>
+    
+    <fieldset>
+ <div class="row">
+ <div class="form-group col-4">
+            <label for="InputPseudo" class="form-label mt-4">Votre pseudo</label>
+            <input type="text" class="form-control" id="InputPseudo" aria-describedby="pseudo" placeholder="Enter votre pseudo">
+            <small id="emailHelp" class="form-text text-muted"></small>
+        </div>
+
+        <div class="form-group col-8">
+            <label for="Message" class="form-label mt-4">votre message</label>
+            <textarea class="form-control" id="Message" rows="3">Taper votre message</textarea>
+        </div>
+</div>    
+        <div>    
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </fieldset>    
+</form> 
+  
+
+
+<?php
+
+?>
---
+++ MonSuperChat/index.php
@@ -0,0 +1,4 @@
+<?php
+require 'View/Default.php';
+
+?>
